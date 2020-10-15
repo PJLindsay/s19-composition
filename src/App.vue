a76c1be6660757530adff3f8fb2200a0ab1dfaed
PJ Lindsay <pjlindsay@gmail.com>
2020-10-15 12:18:25 -0600

    Refactor to use raw object

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
   </section>
 </template>
 
@@ -8,15 +9,27 @@
 import { ref } from 'vue'
 
 export default {
+  // NEW WAY: (Composition API)
   setup() {
-    const uName = ref('Maximilian') // create a reference (a reactive value we can use in our template)
+    // const uName = ref('Maximilian') // create a reference (a reactive value we can use in our template)
+    // const uAge = ref(31)
+
+    const user = ref({
+      name: 'Maximilian',
+      age: 31
+    })
 
     setTimeout(function() {
-      uName.value = 'Max'
+      // uName.value = 'Max'
+      // uAge.value = 32
+      user.value.name = 'Max'
+      user.value.age = 32
     }, 2000)
 
-    return { userName: uName} // return whatever you want to expose to template
+    // passing 'raw' object (user) lets template use this in a reactive way
+    return { userName: user.value.name, age: user.value.age, user: user } // return whatever you want to expose to template
   },
+  // OLD WAY: (Options API)
   // data() {
   //   return {
   //     userName: 'Maximilian',
